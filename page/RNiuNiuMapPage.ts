e224e65e43e2a55063aef75187c4e05e5e110d4f
happy <happy>
2020-01-04 15:55:23 +0800

    client批量提交

--- page/RNiuNiuMapPage.ts
+++ page/RNiuNiuMapPage.ts
@@ -74,6 +74,7 @@ module gamerniuniu.page {
                 PathGameTongyong.atlas_game_ui_tongyong + "fk.atlas",
                 PathGameTongyong.atlas_game_ui_tongyong + "chongzhi.atlas",
                 PathGameTongyong.atlas_game_ui_tongyong + "nyl.atlas",
+                PathGameTongyong.atlas_game_ui_tongyong + "yq.atlas",
                 PathGameTongyong.atlas_game_ui_tongyong_general + "effect/suiji.atlas",
                 PathGameTongyong.atlas_game_ui_tongyong_general + "effect/fapai_1.atlas",
                 PathGameTongyong.atlas_game_ui_tongyong_general + "effect/xipai.atlas",
